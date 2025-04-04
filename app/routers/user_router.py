29204cad9cd39a39bee5f3eb709779fad87f0be5
SaiMarnPha <saimarpha@gmail.com>
2025-04-05 03:52:57 +0700

    Add Header Logo Link and Main page endpoint in user rotuer

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -7,10 +7,22 @@ from fastapi import Depends, Request, Body
 from sqlalchemy.ext.asyncio import AsyncSession
 from services.user_service import UserService
 from repositories.user_model import UserRole
+from fastapi.responses import RedirectResponse
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+@router.get("/", response_class=HTMLResponse)
+async def get_user(request: Request, db: AsyncSession = Depends(get_db)):
+    user = request.session.get("user_id")
+    if not user:
+        return templates.TemplateResponse("index.html", {"request": request})
+    user = await AuthLogic.get_current_user(request=request, db=db)
+    if user.role == UserRole.participant:
+        return RedirectResponse(url="/participants/dashboard", status_code=302)
+    elif user.role == UserRole.questioner:
+        return RedirectResponse(url="/questioners/dashboard", status_code=302)
 
 @router.get("/me/profile", response_class=HTMLResponse)
 async def get_user_profile(request: Request, db: AsyncSession = Depends(get_db)):
